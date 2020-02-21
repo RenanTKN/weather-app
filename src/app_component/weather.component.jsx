367bd3059773f2ac78ab204b07fb3ab7014f24d8
renantkn <renanwds@gmail.com>
2020-02-21 00:14:45 -0300

    Update code and css

--- src/app_component/weather.component.jsx
+++ src/app_component/weather.component.jsx
@@ -1,32 +1,30 @@
 import React from "react";
 import Loading from "./loading.component";
 
-const Weather = props => {
-  return (
-    <div className="container">
-      {props.loading ? (
-        <Loading />
-      ) : props.notFound ? (
-        <div className="text-light weather-data">
-          <h1>City not found</h1>
-        </div>
-      ) : (
-        props.city && (
-          <div className="text-light weather-data">
-            <h1>{props.city}</h1>
-            <h5 className="py-3">
-              <i className={`wi ${props.weatherIcon} display-1`}></i>
-            </h5>
-            {props.tempCelsius ? (
-              <h1 className="py-2">{props.tempCelsius}&deg;</h1>
-            ) : null}
-            {minMaxTemp(props.tempMin, props.tempMax)}
-            <h4 className="py-3">{props.description}</h4>
-          </div>
-        )
-      )}
-    </div>
-  );
+function Weather(props) {
+  if (props.loading) {
+    return <Loading />;
+  } else if (props.notFound) {
+    return (
+      <div className="text-light weather-data">
+        <h1>City not found</h1>
+      </div>
+    );
+  } else {
+    return props.city ? (
+      <div className="text-light weather-data">
+        <h1>{props.city}</h1>
+        <h5 className="py-3">
+          <i className={`wi ${props.weatherIcon} display-1`}></i>
+        </h5>
+        {props.tempCelsius ? (
+          <h1 className="py-2">{props.tempCelsius}&deg;</h1>
+        ) : null}
+        {minMaxTemp(props.tempMin, props.tempMax)}
+        <h4 className="py-3">{props.description}</h4>
+      </div>
+    ) : '';
+  }
 };
 
 function minMaxTemp(min, max) {
